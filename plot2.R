5cb1cccf890d27f767df26e62e31e501654b1e81
aragithub <araceli.cruz@gmail.com>
2015-11-08 03:35:52 -0600

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,23 @@
+#PLOT 2
+
+
+setwd("~/Desktop/COURSERA/MODULO4/proyecto1")
+
+base<-read.table("household_power_consumption.txt",sep=";",header=T,
+                 colClasses=c("character","character","double","double",
+                              "double","double","double","double","numeric"),
+                 na.strings="?")
+
+
+base$DateTime <- strptime(paste(base$Date, base$Time), "%d/%m/%Y %H:%M:%S")
+baseFinal <- subset(base, as.Date(DateTime) >= as.Date("2007-02-01") & 
+                      as.Date(DateTime) <= as.Date("2007-02-02"))
+
+
+png("plot2.png", width = 480, height = 480)
+
+plot(baseFinal$DateTime, baseFinal$Global_active_power, xlab = "", 
+     ylab = "Global Active Power (kilowatts)",type="n")
+lines(baseFinal$DateTime, baseFinal$Global_active_power)
+
+dev.off ()
